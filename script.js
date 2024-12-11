cd1b516eab5fe3e74c8317726f3b675bb230d6a0
mitaliradia <158816169+mitaliradia@users.noreply.github.com>
2024-12-11 16:36:57 +0530

    Adding script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+async function fetchWeatherData(){
+    const city = document.getElementById('city').ariaValueMax.trim();
+    if(!city){
+        alert('Please enter a city name.');
+        return;
+    }
+}
